a9c54fa441c7c306e813e46d7bff65cc8bc701e5
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2018-01-21 10:49:31 +0700

    Add: icon in ticker price

--- src/CoinPrice.js
+++ src/CoinPrice.js
@@ -16,6 +16,11 @@ const FadeIn = styled.div`
   animation-duration: 0.5s;
 `;
 
+const Icon = ({ symbol }) => {
+  const source = require(`./icons/svg/color/${symbol.toLowerCase()}.svg`);
+  return <img className="icon" src={source} />;
+};
+
 const Heading = ({ rank, name, symbol }) => 
   <p className="heading">#{ rank }. {name} ({symbol}) </p>;
 
@@ -52,6 +57,7 @@ export default class CoinPrice extends Component {
         <FadeIn>
           <Heading rank={rank} name={name} symbol={symbol} />
           <p className="title" >
+            <Icon symbol={symbol} />
             <Price value={price} currencySymbol="Rp" />
             <PercentChange value={percentage} />
           </p>
@@ -67,6 +73,10 @@ CoinPrice.propTypes = {
   symbol : PropTypes.string,
   price: PropTypes.number,
   percentage: PropTypes.number
+};
+
+Icon.propTypes = {
+  symbol: PropTypes.string
 };
 
 Heading.propTypes = {
